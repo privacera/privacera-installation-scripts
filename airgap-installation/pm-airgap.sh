30dc679d7045acde829cd1be27528b794d158a9c
Ankit Soni <ankit.soni@privacera.com>
2025-08-14 11:37:00 +0530

    DG-12038 updating airgap bootstrap script.

--- airgap-installation/pm-airgap.sh
+++ airgap-installation/pm-airgap.sh
@@ -8,6 +8,44 @@ PRIVACERA_VERSION=$(echo $PRIV_MGR_IMAGE | awk -F':' '{print $2}')
 PRIVACERA_BASE_DOWNLOAD_URL=$(echo $PRIV_MGR_PACKAGE | awk -F/ '{print $1 "//" $3}')
 AIRGAP_SCRIPT_DOWNLOAD_URL=$PRIVACERA_BASE_DOWNLOAD_URL/privacera-manager/$PRIVACERA_VERSION/pm-airgap-installation-v2.sh
 
+########################################################
+########################################################
+docker_login() {
+  local hub_url="$1"
+  local username="$2"
+  local password="$3"
+
+  if [[ -z "$hub_url" || -z "$username" || -z "$password" ]]; then
+    echo "Error: Missing required arguments in docker login."
+    exit 1
+  fi
+
+  echo "Logging into Docker Hub at $hub_url as $username user."
+  echo "$password" | docker login "$hub_url" -u "$username" --password-stdin
+
+  if [[ $? -eq 0 ]]; then
+    echo "Docker Login successful."
+  else
+    echo "Docker Login failed. Please verify the details and retry."
+    exit 1
+  fi
+  echo "==========================================================="
+}
+PRIVACERA_HUB_URL=$(echo $PRIV_MGR_IMAGE | awk -F'/' '{print $1}')
+docker_login $PRIVACERA_HUB_URL $PRIVACERA_HUB_USER $PRIVACERA_HUB_PASSWORD
+docker_login $DESTINATION_HUB_URL $DESTINATION_HUB_USER $DESTINATION_HUB_PASSWORD
+
+echo "Pulling the Privacera Manager Image from Privacera Hub..."
+docker pull $PRIV_MGR_IMAGE
+docker tag $PRIV_MGR_IMAGE $DESTINATION_HUB_URL/privacera-manager:$PRIVACERA_VERSION
+echo "==========================================================="
+echo "Pushing the Privacera Manager Image to Destination Hub..."
+docker push $DESTINATION_HUB_URL/privacera-manager:$PRIVACERA_VERSION
+echo "==========================================================="
+
+########################################################
+########################################################
+
 echo "Downloading/Updating the Airgap Script..."
 wget -c $AIRGAP_SCRIPT_DOWNLOAD_URL -O pm-airgap-installation-v2.sh
 chmod +x pm-airgap-installation-v2.sh
